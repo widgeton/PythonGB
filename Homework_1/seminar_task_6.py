c4d3395377e2e680be5c9b1d5eddf5481a3ff74f
Anton Vashenko <vashchenko@mail.ru>
2023-06-03 11:45:01 +0300

    Fixed flaws homework #1 after review.

--- Homework_1/seminar_task_6.py
+++ Homework_1/seminar_task_6.py
@@ -5,7 +5,7 @@
 
 year = int(input('Введите год: '))
 
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     result = 'високосный'
 else:
     result = 'не високосный'
--- Homework_1/seminar_task_9.py
+++ Homework_1/seminar_task_9.py
@@ -4,14 +4,14 @@ i = 2
 first_row = ''
 second_row = ''
 while i <= 10:
-    first_row += f'2 x {i} = {2 * i}\t\t' \
-                 f'3 x {i} = {3 * i}\t\t' \
-                 f'4 x {i} = {4 * i}\t\t' \
-                 f'5 x {i} = {5 * i}\n'
-    second_row += f'6 x {i} = {6 * i}\t\t' \
-                  f'7 x {i} = {7 * i}\t\t' \
-                  f'8 x {i} = {8 * i}\t\t' \
-                  f'9 x {i} = {6 * i}\n'
+    first_row += f'2 x {i:<2d}= {2 * i:>2d}\t\t' \
+                 f'3 x {i:<2d}= {3 * i:>2d}\t\t' \
+                 f'4 x {i:<2d}= {4 * i:>2d}\t\t' \
+                 f'5 x {i:<2d}= {5 * i:>2d}\n'
+    second_row += f'6 x {i:<2d}= {6 * i:>2d}\t\t' \
+                  f'7 x {i:<2d}= {7 * i:>2d}\t\t' \
+                  f'8 x {i:<2d}= {8 * i:>2d}\t\t' \
+                  f'9 x {i:<2d}= {6 * i:>2d}\n'
     i += 1
 
 print(first_row, second_row, sep='\n')
